5896151f1052dbac65a82567c06f187d64f98c89
Jadira Palacios <tpalaciosayala@gmail.com>
2025-07-05 12:35:42 -0500

    Primer Lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TercerRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Jadira Palacios",                         # Tu nombre
+    author_email="tpalaciosayala@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/JadiraPalacios/TercerReporCG",     # URL del proyecto
+)
